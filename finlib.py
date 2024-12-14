2ae9cdc724bbc81d40567ce692800323b1de80bb
Rajat Swarup <rajats@gmail.com>
2024-12-14 01:46:58 -0500

    Added more funcs to the lib

--- finlib.py
+++ finlib.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+import scipy
+import scipy.stats
 
 def drawdown(r: pd.Series) -> pd.DataFrame:
     """
@@ -15,7 +17,7 @@ def drawdown(r: pd.Series) -> pd.DataFrame:
         'Drawdown': drawdowns
     })
 
-def get_ffme_returns():
+def get_ffme_returns() -> pd.DataFrame:
     me_m = pd.read_csv('data/Portfolios_Formed_on_ME_monthly_EW.csv',
                        header=0, index_col=0, parse_dates=True, na_values=-99.99)
     rets = me_m [['Lo 10', 'Hi 10']]
@@ -24,3 +26,30 @@ def get_ffme_returns():
     rets.index = pd.to_datetime(rets.index, format='%Y%m').to_period('M')
     return rets
 
+def get_hfi_returns() -> pd.DataFrame:
+    hfi = pd.read_csv('data/edhec-hedgefundindices.csv',header=0, index_col=0, parse_dates=True)
+    hfi = hfi / 100
+    hfi.index = hfi.index.to_period('M')
+    return hfi
+
+def skewness(r: pd.Series) -> pd.Series:
+    demeaned_r = r - r.mean()
+    sigma_r = r.std(ddof=0)
+    exp = (demeaned_r ** 3).mean()
+    return exp/sigma_r**3
+
+def kurtosis(r: pd.Series) -> pd.Series:
+    demeaned_r = r - r.mean()
+    sigma_r = r.std(ddof=0)
+    exp = (demeaned_r ** 4).mean()
+    return exp/sigma_r**4
+
+def is_normal(r: pd.Series, level :float = 0.01 ) -> bool:
+    '''
+    Applies Jarque Bera test
+    :param r:
+    :param level:
+    :return:
+    '''
+    statistic, p_value = scipy.stats.jarque_bera(r)
+    return p_value > level
